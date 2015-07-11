c6a3244f9d7e3d56ecdbbc83b2565eb6d34025c5
Nick-Mazuk <trainridernick@gmail.com>
2015-07-10 20:39:19 -0700

    Quite a bit of work

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var genContent;
 var genAlphabet;
 var genNumbers;
 var genPunctuation;
-var genOtherNonaplhanumericCharacters;
+var genOtherNonalphanumericCharacters;
 var genAlphabetList = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var genNumbersList = "0123456789";
 var genPunctuationList = "!&;:'\",.?()";
@@ -21,6 +21,11 @@ var genSpecificLength;
 var genGenerate;
 var genPassword;
 var valContent;
+var valInput;
+var valPercentage;
+var valScoreBarInner;
+var valLength;
+var valFixes;
 var memContent;
 var memInput;
 var memGuess;
@@ -58,6 +63,13 @@ function onLoad() {
 	genGenerate = document.getElementById("genGenerate");
 	genPassword = document.getElementById("genPassword");
 	valContent = document.getElementById("valContent");
+	valInput = document.getElementById("valInput");
+	valPercentage = document.getElementById("valPercentage");
+	valScoreBarInner = document.getElementById("valScoreBarInner");
+	valLength = document.getElementById("valLength");
+	valVariety = document.getElementById("valVariety");
+	valPatterns = document.getElementById("valPatterns");
+	valFixes = document.getElementById("valFixes");
 	memContent = document.getElementById("memContent");
 	memInput = document.getElementById("memInput");
 	memGuess = document.getElementById("memGuess");
@@ -84,6 +96,7 @@ function onLoad() {
 	memRestart.addEventListener("click", function(){memNewPassword()});
 	about.addEventListener("click", function(){toggleAbout()});
 	memPass.style.display = "none";
+	valInput.onkeypress = function(e){if (!e) e = window.event;var keyCode = e.keyCode || e.which;if (keyCode == '13'){valAnalyze();}}
 	memInput.onkeypress = function(e){if (!e) e = window.event;var keyCode = e.keyCode || e.which;if (keyCode == '13'){memStart();}}
 	memGuess.onkeypress = function(e){if (!e) e = window.event;var keyCode = e.keyCode || e.which;if (keyCode == '13'){memCheck();}}
 }
@@ -123,7 +136,7 @@ function genGeneratePassword() {
 	if(genSpecificLength.value != "") {
 		length = parseInt(genSpecificLength.value);
 	} else {
-		var min = 8;
+		var min = 10;
 		var max = 16;
 		if(genMax.value != "") {
 			max = parseInt(genMax.value);
@@ -147,7 +160,12 @@ function genGeneratePassword() {
 		characterList += genOtherNonalphanumericCharactersList;
 	}
 	if(genCustomCharCheck.checked) {
-		characterList += genCustomChar.value;
+		var string = genCustomChar.value;
+		string = deleteChar(string," ");
+		for(i = 0; i < characterList.length; i++) {
+			string = deleteChar(string,characterList.substring(i,i+1));
+		}
+		characterList += string;
 	}
 	var output = "";
 	for(i = 0; i < length; i++) {
@@ -155,6 +173,112 @@ function genGeneratePassword() {
 		output += characterList.substring(location,location + 1);
 	}
 	genPassword.innerHTML = output;
+}
+
+function valAnalyze() {
+	var password = valInput.value;
+	valFixes.innerHTML = "";
+	var lengthScore = valAnalyzeLength(password);
+	var varietyScore = valAnalyzeVariety(password);
+	var wordScore = valAnalyzePatterns(password);
+	var scores = [lengthScore,varietyScore,wordScore];
+	var score = Math.round(average(scores));
+	valScoreBarInner.style.width = score + "%";
+	if(score <= 60) {
+		valPercentage.style.color = "#CC2200";
+	} else if(score <=75) {
+		valPercentage.style.color = "#CCCC00";
+	} else if(score < 85) {
+		valPercentage.style.color = "#99CC00";
+	} else if(score < 95) {
+		valPercentage.style.color = "#55CC00";
+	} else {
+		valPercentage.style.color = "#00CC00";
+	}
+	valPercentage.innerHTML = score + "%";
+}
+
+function valAnalyzeLength(password) {
+	var score = (password.length / 10 * 100);
+	if(score > 100) {
+		score = 100;
+	}
+	valLength.innerHTML = "Length: " + score + "%";
+	if(password.length < 10) {
+		valFixes.innerHTML += "Short password, add at least " + (10 - password.length) + " characters<br>"
+	}
+	return score;
+}
+
+function valAnalyzeVariety(password) {
+	var score = 0;
+	score += valAnalyzeVarietyHelper(password,genAlphabetList.substring(0,26),"lower case letters"); //gets lower case
+	score += valAnalyzeVarietyHelper(password,genAlphabetList.substring(26,genAlphabetList.length),"capital letters"); //gets Upper Case
+	score += valAnalyzeVarietyHelper(password,genNumbersList,"numbers"); //gets numbers
+	score += valAnalyzeVarietyHelper(password,genPunctuationList + genOtherNonalphanumericCharactersList,"non alphanumeric characters");
+	score = score / 8 * 100
+	valVariety.innerHTML = "Variety: " + Math.round(score) + "%";
+	return score;
+}
+
+function valAnalyzeVarietyHelper(password,string,type) {
+	var count = 0;
+	for(i = 0; i < string.length; i++) {
+		if(password.indexOf(string.substring(i,i+1)) != -1) {
+			count++;
+		}
+		if(count >= 2) {
+			break;
+		}
+	}
+	if(count < 2) {
+		valFixes.innerHTML += "If possible, add " + (2 - count) + " more " + type + "<br>";
+	}
+	return count;
+}
+
+function valAnalyzePatterns(password) {
+	var checkList = [commonWords];
+	password = password.toLowerCase();
+	var count = 0;
+	for(i = 0; i < checkList.length; i++) {
+		for(j = 0; j < checkList[i].length; j++) {
+			if(checkList[i][j].length <= 2) {
+				continue;
+			}
+			if(password.indexOf(checkList[i][j].toLowerCase()) != -1) {
+				count++;
+				valFixes.innerHTML += "Remove the word \"" + checkList[i][j].toLowerCase() + "\"<br>";
+			}
+			var leet = valToLeet(checkList[i][j].toLowerCase());
+			for(k = 1; k < leet.length; k++) {
+				if(password.indexOf(leet[k]) != -1) {
+					valFixes.innerHTML += "Remove the Leet Speak version of \"" + leet[0] + "\"<br>";
+				}
+			}
+		}
+	}
+	console.log(count);
+	return 50;
+}
+
+function valToLeet(word) {
+	var list = [word];//figure out a way to make a single word be all permutations of L33T SP34k, including different substitutions when possible.
+	var leetList = [["o","0"],["e","3"]];
+	for(l = 0; l < word.length; l++) {
+		for(m = 0; m < leetList.length; m++) {
+			if(leetList[m][0] == word.substring(l,l+1)) {
+				var listLength = list.length;
+				for(n = 0; n < listLength; n++) {
+					for(o = 1; o < leetList[m].length; o++) {
+						list.push(list[n].substring(0,l) + leetList[m][o] + list[n].substring(l+1,word.length));
+					}
+				}
+			}
+			continue;
+		}
+	}
+	return list;
 }
 
 function memStart() {
@@ -306,6 +430,18 @@ function toggleAbout() {
 	}
 }
 
+function deleteChar(string, char) {
+	var i = 0;
+	while(i < string.length) {
+		if(string.substring(i,i+1) == char) {
+			string = string.substring(0,i) + string.substring(i+1,string.length);
+		} else {
+			i++;
+		}
+	}
+	return string;
+}
+
 //code borrowed from Mozilla's X-Tag library http://www.x-tags.org/
 var prefix = (function () {
   var styles = window.getComputedStyle(document.documentElement, ''),
@@ -346,3 +482,6 @@ var skipTransition = function(element, fn, bind){
 };
 
 //end borrowed code
+
+//some long variables that would make the code look messy if they were up top
+var commonWords = ["Ailurophile", "Assemblage", "Becoming", "Beleaguer", "Brood", "Bucolic", "Bungalow", "Bungalow", "Comely", "Conflate", "Cynosure", "Dalliance", "Demesne", "Demure", "Denouement", "Desuetude", "Desultory", "Diaphanous", "Dissemble", "Dulcet", "Ebullience", "Effervescent", "Efflorescence", "Elision", "Elixir", "Eloquence", "Embrocation", "Emollient", "Ephemeral", "Epiphany", "Erstwhile", "Ethereal", "Evanescent", "Evocative", "Fetching", "Felicity", "Forbearance", "Fugacious", "Furtive", "Gambol", "Glamour", "Gossamer", "Halcyon", "Harbinger", "Imbrication", "Imbroglio", "Imbue", "Incipient", "Ineffable", "Ingénue", "Inglenook", "Insouciance", "Inure", "Labyrinthine", "Lagniappe", "Lagoon", "Languor", "Lassitude", "Leisure", "Lilt", "Lissome", "Lithe", "Love", "Mellifluous", "Moiety", "Mondegreen", "Murmurous", "Nemesis", "Offing", "Onomatopoeia", "Opulent", "Palimpsest", "Panacea", "Panoply", "Pastiche", "Penumbra", "Petrichor", "Plethora", "Propinquity", "Pyrrhic", "Quintessential", "Ratatouille", "Ravel", "Redolent", "Riparian", "Ripple", "Scintilla", "Sempiternal", "Seraglio", "Serendipity", "Summery", "Sumptuous", "Surreptitious", "Susquehanna", "Susurrous", "Talisman", "Tintinnabulation", "Umbrella", "Untoward", "Vestigial", "Wafture", "Wherewithal", "Woebegone", "Supercalifragilisticexpialidocious", "The", "Be", "And", "Of", "A", "In", "To", "Have", "It", "I", "That", "For", "You", "He", "With", "On", "Do", "Say", "This", "They", "At", "But", "We", "His", "From", "That", "Not", "By", "She", "Or", "As", "What", "Go", "Their", "Can", "Who", "Get", "If", "Would", "Her", "All", "My", "Make", "About", "Know", "Will", "As", "Up", "One", "Time", "Their", "Year", "So", "Think", "When", "Which", "Them", "Some", "Me", "People", "Take", "Out", "Into", "Just", "See", "Him", "Your", "Come", "Could", "Now", "Then", "Like", "Other", "How", "Then", "Its", "Our", "Two", "More", "These", "Want", "Way", "Look", "First", "Also", "New", "Because", "Day", "More", "Use", "No", "Man", "Find", "Here", "Thing", "Give", "Many", "Well", "Only", "Those", "Tell", "One", "Very", "Her", "Even", "Back", "Any", "Woman", "Good", "Through", "Us", "Life", "Child", "There", "Work", "Down", "May", "After", "Should", "Call", "Would", "Over", "School", "Still", "Try", "In", "Ask", "Last", "Need", "Too", "Feel", "Three", "When", "State", "Never", "Become", "Between", "High", "Really", "Something", "Most", "Another", "Much", "Family", "Own", "Leave", "Put", "old", "While", "Mean", "Keep", "Student", "Why", "Let", "Great", "Same", "Big", "Group", "Begin", "Seem", "Country", "Help", "Talk", "Where", "Turn", "Problem", "Every", "Start", "Hand", "Might", "American", "Show", "Part", "About", "Against", "Place", "Over", "Such", "Again", "Few", "Case", "Most", "Week", "Company", "Where", "System", "Each", "Right", "Program", "Hear", "Question", "During", "Work", "Play", "Government", "Run", "Small", "Number", "Off", "Always", "Move", "Like", "Night", "Live", "Mr.", "Point", "Believe", "Hold", "Today", "Bring", "Happen", "Next", "Without", "Before", "Large", "All", "Million", "Must", "Home", "Under", "Water", "Room", "Write", "Mother", "National", "Area", "Money", "Story", "ASL", "Young", "Fact", "Month", "Different", "Lot", "Study", "Book", "Eye", "Job", "Work", "Though", "Business", "Issue", "Side", "Kind", "Four", "Head", "Far", "Black", "Long", "Both", "Little", "House", "Yes", "After", "Since", "Long", "Provide", "Service", "Around", "Friend", "Important", "Father", "Sit", "Away", "Until", "Power", "Hour", "Game", "Often", "Yet", "Line", "Political", "End", "Among", "Ever", "Stand", "Bad", "Loose", "However", "Member", "Pay", "Law", "Meet", "Car", "City", "Almost", "Include", "Continue", "Set", "Later", "Community", "Much", "Name", "Five", "Once", "White", "Least", "President", "Learn", "Real", "Change", "Team", "Minute", "Best", "Several", "Idea", "Kid", "Body", "Information", "Nothing", "Ago", "Right", "Lead", "Social", "Understand", "Whether", "Back", "Watch", "Together", "Follow", "Around", "Parent", "Only", "Stop", "Face", "Anything", "Create", "Public", "Already", "Speak", "Others", "Read", "Level", "Allow", "Add", "Office", "Spend", "Door", "Health", "Person", "Art", "Sure", "Such", "War", "History", "Party", "Within", "Grow", "Result", "Open", "Change", "Morning", "Walk", "Reason", "Low", "Win", "Research", "Girl", "Guy", "Early", "Food", "Before", "Moment", "Himself", "Air", "Teacher", "Force", "Offer", "Enough", "Both", "Education", "Across", "Although", "Remember", "Foot", "Second", "Boy", "Maybe", "Toward", "Able", "Age", "Off", "Policy", "Everything", "Process", "Music", "Including", "Appear", "Consider", "Actually", "Buy", "Probably", "Human", "Wait", "Serve", "Market", "Die", "Send", "Expect", "Home", "Sense", "Build", "Stay", "Fall", "Oh", "Nation", "Plan", "Cut", "College", "Interest", "Death", "Course", "Someone", "Experience", "Behind", "Reach", "Local", "Kill", "Six", "Remain", "Effect", "Use", "Yeah", "Suggest", "Class", "Control", "Raise", "Care", "Perhaps", "Little", "Late", "Hard", "Field", "Else", "Pass", "Former", "Sell", "Major", "Somethimes", "Require", "Along", "Development", "Themselves", "Report", "Role", "Better", "Economic", "Effort", "Up", "Decide", "Rate", "Strong", "Possible", "Heart", "Show", "Leader", "Light", "Voice", "Wife", "Whole", "Police", "Mind", "Finally", "Return", "Pull", "Free", "Military", "Price", "Report", "Less", "According", "Decision", "Explain", "Son", "Hope", "Even", "Develop", "View", "Relationship", "Carry", "Town", "Road", "Drive", "Arm", "True", "Federal", "Break", "Better", "Difference", "Thank", "Recieve", "Value", "International", "Building", "Action", "Full", "Model", "Join", "Season", "Society", "Because", "Tax", "Director", "Early", "Position", "Player", "Agree", "Especially", "Accord", "Pick", "Wear", "Paper", "Special", "Space", "Ground", "Form", "Support", "Event", "Official", "Whose", "Matter", "Everyone", "Center", "Couple", "Site", "End", "Project", "Hit", "Base", "Activity", "Star", "Table", "Need", "Court", "Produce", "Eat", "Teach", "Oil", "Half", "Situation", "Easy", "Cost", "Industry", "Figure", "Face", "Street", "Image", "Itself", "Phone", "Either", "Data", "Cover", "Quite", "Picture", "Clear", "Practice", "Piece", "Land", "Recent", "Describe", "Product", "Docter", "Wall", "Patient", "Docter Who?", "Worker", "News", "Test", "Movie", "Certain", "North", "Personal", "Open", "Simply", "Third", "Technology", "Catch", "Step", "Baby", "Computer", "Type", "Attention", "Draw", "Film", "Tree", "Source", "Red", "Nearly", "Organization", "Choose", "Hair", "Cause", "Look", "Point", "Century", "Evidence", "Window", "Difficult", "Listen", "Soon", "Culture", "Billion", "Chance", "Brother", "Energy", "Period", "Course", "Summer", "Less", "Realize", "Hundred", "Available", "Plant", "Likely", "Opportunity", "Term", "Short", "Letter", "Condition", "Choice", "Place", "Single", "Rule", "Daughter", "Administration", "South", "Husband", "Congress", "Floor", "Campaign", "Material", "Population", "Well", "Call", "Economy", "Medical", "Hospital", "Church", "Close", "Thousand", "Risk", "Current", "Fire", "Future", "Wrong", "Involve", "Defense", "Anyone", "Increase", "Security", "Bank", "Myself", "Certainly", "West", "Sport", "Board", "Seek", "Per", "Subject", "Officer", "Private", "Rest", "Behavior", "Deal", "Performance", "Fight", "Throw", "Top", "Quickly", "Past", "Goal", "Bed", "Second", "Order", "Author", "Fill", "Represent", "Focus", "Foreign", "Drop", "Plan", "Blood", "Upon", "Agency", "Push", "Nature", "Color", "No", "Recently", "Store"]; //700,  http://www.englishclub.com/vocabulary/common-words-5000.htm
